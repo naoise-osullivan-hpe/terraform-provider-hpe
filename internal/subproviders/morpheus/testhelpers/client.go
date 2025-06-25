ad984583fc280d02f83525fa207e277a3776c8ec
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-06-25 16:58:56 +0100

    Enable use of access token in testhelpers client (#107)
    So that we aren't limited to credentials when running acceptance tests.

--- internal/subproviders/morpheus/testhelpers/client.go
+++ internal/subproviders/morpheus/testhelpers/client.go
@@ -20,6 +20,6 @@ func newClient(ctx context.Context, t *testing.T) *sdk.APIClient {
 		os.Getenv("TF_VAR_testacc_morpheus_url"),
 		os.Getenv("TF_VAR_testacc_morpheus_username"),
 		os.Getenv("TF_VAR_testacc_morpheus_password"),
-		"",
+		os.Getenv("TF_VAR_testacc_morpheus_access_token"),
 		clientfactory.WithInsecureTLS())
 }
